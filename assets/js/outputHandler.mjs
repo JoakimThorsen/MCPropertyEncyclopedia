c010d8fc9c461657645e318cc429ee237a84fdd7
JoakimThorsen <joakimkvarme.thorsen@icloud.com>
2024-03-22 00:02:44 +0100

    small visual tweaks

--- assets/js/outputHandler.mjs
+++ assets/js/outputHandler.mjs
@@ -80,19 +80,21 @@ function headerOutputter(page, entry_header) {
                                 <i class="fas fa-sort-amount-up"></i>
                             </button>
                         </span>
-                        <button class="btn dropdown-btn btn-default export-csv"
-                            title="Download a newline-separated list of all blocks that are currently shown">
-                            <i class="fas fa-file-export"></i>Export CSV
-                        </a>
-                        <button class="btn dropdown-btn btn-default export-json"
-                            title="Download a JSON string-array-formatted list of all blocks that are currently shown">
-                            <i class="fas fa-file-export"></i>Export JSON
-                        </a>
-                        <button class="btn dropdown-btn btn-default copy-comma-separated"
-                            title="Copy a Comma-Separated list of all blocks that are currently shown to clipboard.">
-                            <i class="fas fa-copy"></i>Copy Comma-Separated List
-                        </a>
                     </div>
+                </li>
+                <li class="text-center">
+                    <button class="btn dropdown-btn btn-default export-csv"
+                        title="Download a newline-separated list of all blocks that are currently shown">
+                        <i class="fas fa-file-export"></i>Export CSV
+                    </a>
+                    <button class="btn dropdown-btn btn-default export-json"
+                        title="Download a JSON string-array-formatted list of all blocks that are currently shown">
+                        <i class="fas fa-file-export"></i>Export JSON
+                    </a>
+                    <button class="btn dropdown-btn btn-default copy-comma-separated"
+                        title="Copy a Comma-Separated list of all blocks that are currently shown to clipboard.">
+                        <i class="fas fa-copy"></i>Copy Comma-Separated List
+                    </a>
                 </li>
             </ul>
         </div>
